fafdc9b45a4a2604c3b281734435f7d9688f862d
Aline Vita <131705047+GlauciaVita@users.noreply.github.com>
2023-05-24 14:57:55 -0300

    Add files via upload

--- Forca.py
+++ Forca.py
@@ -3,5 +3,27 @@ def jogar():
     print('***Bem vindo ao meu joguinho de forca***')
     print('****************************************')
 
+    palavra_secreta = 'caneta'
+    errou = False
+    acertou = False
+
+    while (not errou and not acertou):
+
+        tentativa = input('Digite uma letra: ').strip()
+      
+        index = 0
+        for letra in palavra_secreta:
+            if(tentativa.upper() == letra.upper()):
+                print('Acertou a letra {} na posição {}'.format(letra, index))
+            index = index + 1
+        print('Jogando...')
+
+
+
+
+    print('Fim de jogo!')
+
+
 jogar()
+
 
